9d6f59a24f328fc1470530f63b4caf63082e7ceb
Silas P Ladislau <silas.ladislau@ale.ro.gov.br>
2022-03-11 08:55:25 -0400

    finished

--- App.js
+++ App.js
@@ -1,38 +1,60 @@
 import React, {useState} from 'react';
-import {View, Switch, StyleSheet, Text, Button} from 'react-native';
+import {View, Switch, StyleSheet, Text, TouchableOpacity} from 'react-native';
 
 import {Input} from './src/components/Input';
 
+const DOLLAR_PRICE = 5.07;
+
 const App = () => {
-  const [inverter, setInverter] = useState(true);
-  const [firstValue, setFirstValue] = useState('20');
-  const [secondValue, setSecondValue] = useState(0.0);
+  const [inverter, setInverter] = useState(false);
+  const [firstValue, setFirstValue] = useState('');
+  const [result, setResult] = useState(0.0);
 
   const convert = () => {
-    setSecondValue((Number(firstValue) / 5.07).toFixed(2));
+    if (firstValue !== '') {
+      if (!inverter) {
+        setResult((Number(firstValue) / DOLLAR_PRICE).toFixed(2));
+      } else {
+        setResult((Number(firstValue) * DOLLAR_PRICE).toFixed(2));
+      }
+      setFirstValue('');
+    } else {
+      alert('Preencha com um valor');
+    }
   };
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Conversor de Moedas</Text>
+
       <View style={styles.row}>
         <Text style={styles.highlight}>Real</Text>
-        <Switch value={inverter} onChange={() => setInverter(!inverter)} />
+        <Switch
+          value={inverter}
+          onChange={() => {
+            setInverter(!inverter);
+            setResult(0.0);
+          }}
+          thumbColor={inverter ? '#aad835' : '#fdd835'}
+        />
         <Text style={styles.highlight}>Dólar</Text>
       </View>
+
       <Input
+        currency={inverter ? 'U$' : 'R$'}
         onChangeText={val => setFirstValue(val)}
         value={firstValue}
         keyboardType="number-pad"
       />
-      <Button
-        style={styles.button}
-        onPress={() => convert()}
-        title="Converter"
-        color="#2E2759"
-      />
+
+      <TouchableOpacity style={styles.button} onPress={() => convert()}>
+        <Text style={styles.buttonText}>Converter</Text>
+      </TouchableOpacity>
+
       <View style={styles.result}>
-        <Text style={styles.secondValue}>{`U$ ${secondValue}`}</Text>
+        <Text style={styles.resultValue}>{`${
+          inverter ? 'R$' : 'U$'
+        } ${result}`}</Text>
       </View>
     </View>
   );
@@ -47,15 +69,23 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 40,
   },
-  button: {
-    margin: 24,
-  },
   row: {
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'flex-end',
     marginTop: 24,
     marginBottom: 24,
+  },
+  button: {
+    alignItems: 'center',
+    marginTop: 8,
+    padding: 10,
+    backgroundColor: '#2E2759',
+  },
+  buttonText: {
+    color: '#b0a4ff',
+    fontWeight: 'bold',
+    textTransform: 'uppercase',
   },
   highlight: {
     color: '#b0c4de',
@@ -66,7 +96,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
-  secondValue: {
+  resultValue: {
     fontSize: 24,
     color: '#b0c4de',
   },
